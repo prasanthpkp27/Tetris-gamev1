7e81bdda5dd7e9f6d53186e2531a9b086afce5db
prasanthpkp27 <42651983+prasanthpkp27@users.noreply.github.com>
2024-04-08 11:09:36 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario1234bucket" # Replace with your actual S3 bucket name
+    bucket = "mariobucket123" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
